9310537ac582b7463d879c5cfbd055689c8b5dd2
alekissandro <alek.rossi@gmail.com>
2021-10-15 16:44:26 -0300

    verso e listview added in home

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -13,14 +13,29 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: NibAppBar(),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 800,
-              height: 400,
-              child: SwiperNibPage(),
-            )
+        child: ListView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 800,
+                  height: 400,
+                  child: SwiperNibPage(),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "E perseveravam na doutrina dos apóstolos, e na comunhão, e no partir do pão, e nas orações.",
+                  style: TextStyle(fontSize: 30, fontFamily: "Birthstone"),
+                ),
+                Text(
+                  "Atos 2:42",
+                  style: TextStyle(fontSize: 30, fontFamily: "Birthstone"),
+                )
+              ],
+            ),
           ],
         ),
       ),
